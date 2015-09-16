6185d733e202194c3f953dbdbcc39d27b3fa60f5
md.sajjad <md.sajjad@gmail.com>
2015-09-16 12:22:15 +0600

    Working Solution
    Working Solution to Pre-work… No Extra Work Done Yet

--- CombienPointe/ViewController.swift
+++ CombienPointe/ViewController.swift
@@ -31,7 +31,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func billAmount_OnEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentage = tipPercentages[tipPercentSegmentCtrlr.selectedSegmentIndex];
+        var currentBillAmt = (billTextField.text as NSString).doubleValue;  // Cheap way ignoring possibility of parsing errors
+        var tip : Double = tipPercentage * currentBillAmt;
+        tipLabel.text = String(format:"$%.2f", tip);
+        totalLabel.text = String(format:"%.2f", currentBillAmt + tip);
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
 }
 
